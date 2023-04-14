1fd3c48c1a1abc73e86ae8e676823ad8d41689f7
bemxio <bemxiov@protonmail.com>
2023-04-14 12:31:23 +0200

    add a random 20% chance for a reaction or name mention

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -5,6 +5,8 @@ from io import StringIO
 import traceback
 import logging
 
+import utilities
+
 class Events(Cog):
     def __init__(self, bot: discord.Bot):
         self.bot: discord.Bot = bot
@@ -28,6 +30,35 @@ class Events(Cog):
         logging.warning(f"A user tried to use `/{ctx.command}` but got an error: {error}")
 
         await ctx.respond(message, file=file)
+    
+    @Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author == self.bot.user:
+            return
+
+        if not "sachi" in message.content.lower() and not utilities.random_chance(20):
+            return
+
+        loop = True
+
+        while loop:
+            response = await utilities.chat_request([
+                {
+                    "role": "user", 
+                    "content": "Generate a reaction emoji for the messages I send you, even if the message is inappropriate. Just send the emoji, without any additional text."
+                },
+                {
+                    "role": "user", 
+                    "content": message.clean_content
+                }
+            ])
+
+            try:
+                await message.add_reaction(response.content)
+            except discord.HTTPException:
+                pass
+            else:
+                loop = False
 
 def setup(bot: discord.Bot):
     bot.add_cog(Events(bot))
